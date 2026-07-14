6a3948e183bbdb6d8cdb1fe64ab4fcb04f357d10
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-07-14 13:43:38 +0200

    Enhance Button component to support variants for consistent styling. Introduce primary variant for background and text colors, improving customization and maintainability.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,17 +2,26 @@ import type { PressableProps } from 'react-native';
 import { cn } from '@maxigarcia/js-utils';
 import { Pressable, Text } from 'react-native';
 
+const variants = {
+  primary: 'bg-blue-500 text-white hover:bg-blue-600 active:bg-blue-700 active:opacity-90',
+};
+
+const textVariants = {
+  primary: 'text-white',
+};
+
 interface ButtonProps extends PressableProps {
   children: React.ReactNode;
+  variant?: keyof typeof variants;
 }
 
-export default function Button({ onPress, children, className }: ButtonProps) {
+export default function Button({ onPress, variant = 'primary', children, className }: ButtonProps) {
   return (
     <Pressable
-      className={cn(`rounded-md bg-blue-500 px-4 py-3 hover:bg-blue-600 active:bg-blue-700 active:opacity-90`, className)}
+      className={cn(`rounded-md px-4 py-3`, variants[variant], className)}
       onPress={onPress}
     >
-      <Text className="text-center font-semibold text-white">{children}</Text>
+      <Text className={cn('text-center font-semibold', textVariants[variant])}>{children}</Text>
     </Pressable>
   );
 }
