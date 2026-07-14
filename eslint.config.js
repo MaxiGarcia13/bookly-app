584debc2f404be47f052e56455086d3c400b5898
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-07-14 12:06:21 +0200

    Refactor ESLint configuration: replace .mjs with .js file, update package dependencies, and remove unused components and scripts.

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import { eslintConfig } from '@maxigarcia/eslint-config';
+
+export default eslintConfig(
+  {
+    react: true,
+    typescript: true,
+    jsx: true,
+  },
+  {
+    ignores: ['node_modules', '.expo', 'assets'],
+  },
+);
--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import { eslintConfig } from "@maxigarcia/eslint-config";
-
-export default eslintConfig({
-  react: true,
-  typescript: true,
-  jsx: true,
-});
