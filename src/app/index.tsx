daab019e5a999db18e5f283a43dc373c35a33746
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-07-14 13:26:39 +0200

    Update app configuration to rename project to "bookly", enhance package metadata, and add a new Pressable component in the main screen.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,17 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
+      <Pressable
+        className="bg-blue-500 p-4 rounded-md hover:bg-blue-600 touch-auto"
+        onPress={() => {
+          console.warn('Pressed');
+        }}
+      >
+        <Text>Press me</Text>
+      </Pressable>
+
       <Text className="text-2xl font-bold text-red-500">
         Edit src/app/index.tsx to edit this screen.
       </Text>
